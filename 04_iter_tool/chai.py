9a1dd4f1c0799a5937c1c03ef3840c9b3d63035d
Affan Shaikh <115262443+affanshaikh-dev@users.noreply.github.com>
2025-02-14 11:49:50 +0000

    iter tools

---
+++ 04_iter_tool/chai.py
@@ -0,0 +1,4 @@
+import time
+print("Chai is here")
+username = "chai"
+print(f"Username: {username}")
---
+++ 04_iter_tool/file.py
@@ -0,0 +1,5 @@
+f = open("04_iter_tool/chai.py", "r")
+# print(f.readline())
+
+for line in f:
+    print(line, end="") 
